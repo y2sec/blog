9ac5b61f504d31b158a1d8342d2a0260bfd90e33
y2sec <bae3835@gmail.com>
2021-02-03 22:19:18 +0900

    add Home display

--- src/main/java/com/y2sec/blog/controller/HomeController.java
+++ src/main/java/com/y2sec/blog/controller/HomeController.java
@@ -1,10 +1,26 @@
 package com.y2sec.blog.controller;
 
+import com.y2sec.blog.service.CategoryService;
+import com.y2sec.blog.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+
+    private final CategoryService categoryService;
+    private final PostService postService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+
+        model.addAttribute("categoryList", categoryService.findCategory());
+        model.addAttribute("postList", postService.findPosts());
+
+        return "home";
+    }
 
 }
